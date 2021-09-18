22a3395647c00585dcf0bc40ab48090472941ef4
snh3003 <shahsama542@gmail.com>
2021-09-18 18:05:06 +0530

    query params and path params

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 # instance or object
 app = FastAPI()
 
 # decorator or route or path
 @app.get("/")
+def index(limit, published: bool, sort: Optional[str] = None):
+    if(published):
+        return {
+            "blogs": " all blogs "
+        }
+    return {"blog": f"{limit} list of blogs "}
 
-def index():
-    return {"blog": "blog list"}
+@app.get('/blog/unpublished')
+def blog_unpublished():
+    return {
+        "data": "list of unpublished blogs"
+    }
 
+# dynamic routing 
 @app.get("/blog/{id}")
 def blog(id: int):
     return {
@@ -22,4 +34,16 @@ def blog_comments(id: int):
         "blog": "blog name",
         "blog_id": id,
         "comments": ['hell', "0"]
+    }
+
+class Blog(BaseModel):
+    id: int
+    title: str
+    published: bool
+
+@app.post("/blog")
+def create(blog: Blog):
+    return {
+        "status": "created",
+        "blog": blog.title,
     }
